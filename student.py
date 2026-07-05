0667005ba280623f59d607d95981983441827cf7
mnoorzai07 <madinanoorzai0327@gmail.com>
2026-07-05 12:54:30 +0430

    Student.py created

---
+++ student.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self, student_id, name, email):
+        self.student_id = student_id
+        self.name = name
+        self.email = email
+        self.courses = []
+
+    def get_id(self):
+        pass
+
+    def get_name(self):
+        pass
+
+    def set_email(self, email):
+        pass
+
+    def enroll_course(self, course_code):
+        pass
+
+    def display_info(self):
+        pass
