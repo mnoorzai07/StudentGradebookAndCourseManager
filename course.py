fbe939275e5d5bb6a49827c163f8c7d85863ae48
mnoorzai07 <madinanoorzai0327@gmail.com>
2026-07-05 13:00:57 +0430

    course.py created

---
+++ course.py
@@ -0,0 +1,19 @@
+# course.py
+class Course:
+    def __init__(self, course_code, course_name):
+        self.course_code = course_code
+        self.course_name = course_name
+        self.students = []
+        self.assessments = []
+
+    def add_student(self, student_id):
+        pass
+
+    def add_assessment(self, assessment):
+        pass
+
+    def find_assessment(self, title):
+        pass
+
+    def display_info(self):
+        pass
