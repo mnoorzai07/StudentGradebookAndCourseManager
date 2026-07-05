a192a855f78eca8c6bf2b24253269f930df5550f
mnoorzai07 <madinanoorzai0327@gmail.com>
2026-07-05 13:02:42 +0430

    assessment.py created

---
+++ assessment.py
@@ -0,0 +1,14 @@
+# assessment.py
+class Assessment:
+    def __init__(self, title, max_score):
+        self.title = title
+        self.max_score = max_score
+
+    def calculate_percentage(self, score):
+        pass
+
+    def grade_message(self, score):
+        pass
+
+    def display_info(self):
+        pass
