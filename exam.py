ba0f85e9125e190caeccffc3cb9b45d0a0dbbd96
mnoorzai07 <madinanoorzai0327@gmail.com>
2026-07-06 18:30:18 +0430

    exam.py details
    some details are added to exam.py.

--- exam.py
+++ exam.py
@@ -4,8 +4,15 @@ class Exam(Assessment):
     def init(self, title, max_score):
         super().__init__(title, max_score)
 
-    def grade_message(self, score):
-        pass
 
     def display_info(self):
-        pass
+        print(f"Exam: {self.title} - Maximum Score: {self.max_score}")
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+
+        if percentage >= 55:
+            return "Passed exam."
+        else:
+            return "Failed exam."
+
