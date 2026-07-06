752a42c8d77b62e1ec2e1d0819a5ca34d119e45d
mnoorzai07 <madinanoorzai0327@gmail.com>
2026-07-06 13:28:17 +0430

    assessment.py details
    function details added to the Assessment Class

--- assessment.py
+++ assessment.py
@@ -1,14 +1,25 @@
-# assessment.py
 class Assessment:
     def __init__(self, title, max_score):
         self.title = title
         self.max_score = max_score
 
     def calculate_percentage(self, score):
-        pass
+        return (score / self.max_score) * 100
 
     def grade_message(self, score):
-        pass
+        percentage = self.calculate_percentage(score)
+
+        if percentage >= 90:
+            return "Outstanding Excellent!!! Absolute mastery and top-tier performance."
+        elif 89 >= percentage >= 80:
+            return "High Excellence!!! Strong capacity with minor room for growth."
+        elif 79 >= percentage >= 70:
+            return "Above Average! Meeting the basic requirements, need refinement."
+        elif 50 >= percentage >= 69:
+            return "Average! You meet the basic requirements,but you need refinement."
+        else:
+            return"Developing! You need significant improvement and focus."
+
 
     def display_info(self):
-        pass
+        print(f"{self.title}: {self.max_score}%")
