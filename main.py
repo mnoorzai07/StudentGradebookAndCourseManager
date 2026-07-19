bf6a180b253381f3b32f2ea7385d0ec47825d97c
mnoorzai07 <madinanoorzai0327@gmail.com>
2026-07-19 13:05:20 +0430

    main.py was revised

--- main.py
+++ main.py
@@ -9,27 +9,20 @@ from gradebook import Gradebook
 # Create the gradebook
 gradebook = Gradebook()
 
-
 # Add student
 student = Student("S001", "Mina Roberts", "minaroberts@gmail.com")
 
 gradebook.add_student(student)
-
-
 
 # Add course
 course = Course("PY101", "Python Programming")
 
 gradebook.add_course(course)
 
-
-
 # Enroll student
 gradebook.enroll_student("S001", "PY101")
 
-
 # Add assessments
-
 quiz = Quiz("Quiz 1", 10)
 
 midterm = Exam("Midterm Exam", 100)
@@ -40,24 +33,16 @@ gradebook.add_assessment("PY101", quiz)
 gradebook.add_assessment("PY101", midterm)
 gradebook.add_assessment("PY101", final_project)
 
-
-
 # Record grades
 gradebook.record_grade(
-    "S001", "PY101", "Quiz 1", 3)
+    "S001", "PY101", "Quiz 1", 9)
 
-gradebook.record_grade("S001", "PY101", "Midterm Exam", 40)
+gradebook.record_grade("S001", "PY101", "Midterm Exam", 98)
 
-gradebook.record_grade("S001", "PY101", "Final Project", 30)
-
-
+gradebook.record_grade("S001", "PY101", "Final Project", 95)
 
 # Calculate average
 average = gradebook.calculate_average("S001", "PY101")
 
-print(f"Average: {average:.2f}%")
-
-
-
-# Show report
+ # Show report
 gradebook.show_report("S001")
